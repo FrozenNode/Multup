97aab14a08f29fadeeafc1ff42acba48fd83d5c7
sketchynix <nixkelly@gmail.com>
2013-05-08 16:56:08 -0600

    Update start.php
    fixed class not found error on CentOS

--- start.php
+++ start.php
@@ -1,9 +1,9 @@
 <?php
 
 Autoloader::namespaces(array(
-  'Multup' => Bundle::path('multup') . 'libraries'
+	'Multup' => Bundle::path('multup').'libraries'
 ));
 
 Autoloader::map(array(
-	'Multup'	=> __DIR__ . DS . 'multup.php'
+	'Multup' =>  __DIR__.'/multup.php'
 ));
